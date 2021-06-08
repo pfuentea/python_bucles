62e6e4c75f59f1baa297b4cea3bdf92f58dbf786
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2021-06-07 22:07:37 -0400

    ciclo for basico ii

--- for_loop_basic2.py
+++ for_loop_basic2.py
@@ -1,40 +1,58 @@
-def countdown(p_desde):    
-    result=[]
-    for i in range(p_desde,-1,-1):
-        result.append(i)
-    return result    
+def biggie_size (p_lista):
+    for v_indice in range(len(p_lista)):
+        if(p_lista[v_indice]  >0):
+            p_lista[v_indice]='BIG'
+    return p_lista
 
-def PrintAndReturn(p_lista):
-    print(p_lista[0])
-    return(p_lista[1])
+def count_positives(p_lista):
+    v_nPositivos=0
+    for v_elementos in p_lista:
+        if v_elementos>0:
+            v_nPositivos+=1
+    p_lista[len(p_lista)-1]=v_nPositivos
+    return p_lista
 
-def FirstAndLen(p_lista):
-    return(p_lista[0]+len(p_lista))
+def sum_total(p_lista):
+    v_suma=0
+    for v_elementos in p_lista:
+        v_suma+=v_elementos
+    return v_suma
 
-def gtSecond(p_lista):
-    v_result=[]
-    if len(p_lista)>3:
-        for val in p_lista:
-            if(val>p_lista[1]):
-                v_result.append(val)
-        print(len(v_result))
-        return v_result
+def promedio(p_lista):    
+    return sum_total(p_lista)/len(p_lista)
+
+def longitud(p_lista):
+    return len(p_lista)
+
+def minimo(p_lista):
+    if len(p_lista) > 0 :
+        v_minimo=p_lista[0]
+        for v_elemento in p_lista:
+            if v_elemento < v_minimo:
+                v_minimo=v_elemento
+        return v_minimo
     else:
         return False
 
-def ThisLonThatVal(p_size,p_val):
-    v_result=[]
-    for indice in range(0,p_size):
-        v_result.append(p_val)
-    return v_result
-#print(countdown(2))
+def maximo(p_lista):
+    if len(p_lista) >0:
+        v_maximo=p_lista[0]
+        for v_elemento in p_lista:
+            if v_elemento < v_maximo:
+                v_maximo=v_elemento
+        return v_maximo
+    else:
+        return False
 
-#print(PrintAndReturn([1,2]))
+def ultimate_analysis(p_lista):
+     return 'total:'+ str(sum_total(p_lista)) + ', promedio:'+ str(promedio(p_lista))+ ', minimo:' + str(minimo(p_lista))+ ', maximo:'+str(maximo(p_lista)) + ', longitud:'+ str(longitud(p_lista))
+    
 
-#print(FirstAndLen([1,2,3,4,5]))
-
-#print(gtSecond([5,2,3,2,1,4]))
-
-#print(gtSecond([5]))
-
-#print(ThisLonThatVal(7,4))
+#print(biggie_size([- 1, 3, 5, -5]))
+#print(count_positives ([- 1,1,1,1]))
+#print(sum_total ([1,2,3,4]))
+#print(promedio ([1,2,3,4]) )
+#print(longitud ([37,2,1, -9]))
+#print( minimo ([37,2,1, -9]))
+#print(maximo ([37,2,1, -9]))
+print(ultimate_analysis ([37,2,1, -9]) )
